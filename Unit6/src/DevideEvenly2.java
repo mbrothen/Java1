be393c1d81da1cbbc77c7c5f4e68b305e317f2b1
mbrothen <mbrothen@gmail.com>
2018-10-27 20:27:37 -0500

    nested for loops

---
+++ Unit6/src/DevideEvenly2.java
@@ -0,0 +1,21 @@
+
+public class DevideEvenly2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int LIMIT = 100;
+		int var;
+		int number;
+		System.out.print(LIMIT + " is evenly divisible by ");
+		for(number = 1; number <= LIMIT; ++number)
+		{
+			System.out.print(number + " is evenly divisible by ");
+			for(var =1; var <= number; ++var)
+				if(number % var == 0)
+					System.out.print(var + " ");
+			System.out.println();			
+		}
+		System.out.println();
+	}
+
+}
