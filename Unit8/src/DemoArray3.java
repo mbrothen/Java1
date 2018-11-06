b71f806376ac043cfb8c106bda724b5114772466
mbrothen <mbrothen@gmail.com>
2018-11-05 18:27:59 -0600

    More 8 and 9

---
+++ Unit8/src/DemoArray3.java
@@ -0,0 +1,13 @@
+
+public class DemoArray3 {
+
+	public static void main(String[] args) {
+		// Demonstrate how arrays work
+		double[] salaries = {6.25, 6.55, 10.25, 18.85};
+
+		System.out.println("Salaries on by one are:");
+		for(int x = 0; x < salaries.length; ++x)
+			System.out.println(salaries[x]);
+	}
+
+}	
